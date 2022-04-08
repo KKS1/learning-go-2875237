0c73e3a9a6bfdac3a5e3ba9fd732f0afdad69cd0
Kanwaljeet Singh Kalra <koolkalra@gmail.com>
2022-04-08 12:33:59 -0700

    Update main.go

--- practice/main.go
+++ practice/main.go
@@ -5,6 +5,23 @@ import (
 )
 
 func main() {
-	var colors = [3]string{"Red", "Green", "Blue"}
+	var colors = []string{"Red", "Green", "Blue"}
+	colors = append(colors, "Purple")
 	fmt.Println(colors)
+
+	colors = append(colors[1:len(colors)])
+	fmt.Println(colors)
+
+	colors = append(colors[:len(colors)-1])
+	fmt.Println(colors)
+
+	numbers := make([]int, 5)
+
+	for i := 0; i < 5; i++ {
+		numbers[i] = 134 + i
+	}
+
+	numbers = append(numbers, 140)
+
+	fmt.Println(numbers)
 }
