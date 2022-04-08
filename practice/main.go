60057b51c761fe94566e6eb04f34d4fb72058b1b
Kanwaljeet Singh Kalra <koolkalra@gmail.com>
2022-04-08 10:43:17 -0700

    Update main.go

--- practice/main.go
+++ practice/main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
+	n := time.Now()
+	fmt.Println("Time now: ", n)
 
-	fmt.Println("Dates and times")
+	t := time.Date(2022, time.April, 8, 10, 37, 0, 0, time.UTC)
+	fmt.Println("Time date: ", t)
+	fmt.Println(t.Format(time.ANSIC))
 
+	parsedTime, _ := time.Parse(time.ANSIC, "Fri Apr  8 10:37:00 2022")
+	fmt.Printf("Parsed time type: %T\n", parsedTime)
 }
