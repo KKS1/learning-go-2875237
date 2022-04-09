45e51e34452c824b3dd38f2e15a560dd375f6a86
Kanwaljeet Singh Kalra <koolkalra@gmail.com>
2022-04-09 02:21:13 -0700

    Update main.go

--- practice/main.go
+++ practice/main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Conditional logic")
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter a number: ")
+	input, _ := reader.ReadString('\n')
+
+	num, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
+
+	if err != nil {
+		panic("Please enter a number")
+	}
+
+	fmt.Printf("You entered: %v\n", num)
+
+	result := ""
+
+	if num < 0 {
+		result = "less than zero"
+	} else if num == 0 {
+		result = "equals zero"
+	} else {
+		result = "greater than zero"
+	}
+
+	fmt.Println(result)
 }
