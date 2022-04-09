d49f98c1af391f80bc278bb11529602e48d2e764
Kanwaljeet Singh Kalra <koolkalra@gmail.com>
2022-04-09 03:26:50 -0700

    Update main.go

--- practice/main.go
+++ practice/main.go
@@ -7,4 +7,21 @@ import (
 func main() {
 	colors := []string{"Red", "Green", "Blue"}
 	fmt.Println(colors)
+
+	for index, v := range colors {
+		fmt.Println(index, v)
+	}
+
+	val := 1
+
+	for val < 5 {
+		fmt.Println(val)
+		val++
+		if val > 3 {
+			goto FOR_LOOP_END
+		}
+	}
+
+FOR_LOOP_END:
+	fmt.Println("For loop ending statement")
 }
