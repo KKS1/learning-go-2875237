ba147395e45e12ac159f4936cbaf214538cec016
Kanwaljeet Singh Kalra <koolkalra@gmail.com>
2022-04-09 02:55:08 -0700

    Update main.go

--- practice/main.go
+++ practice/main.go
@@ -10,4 +10,18 @@ func main() {
 	rand.Seed(time.Now().Unix())
 	dow := rand.Intn(7) + 1
 	fmt.Println("Day", dow)
+
+	var result string
+
+	switch dow {
+	case 1:
+		result = "Its One"
+	case 2:
+		result = "Its Two"
+		// fallthrough
+	default:
+		result = "Its some other day"
+	}
+
+	fmt.Println(result)
 }
