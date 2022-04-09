a675c13772a0228f8582d99d9650948b35f7f405
Kanwaljeet Singh Kalra <koolkalra@gmail.com>
2022-04-09 01:57:55 -0700

    Update main.go

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,13 @@ import (
 )
 
 func main() {
-	fmt.Println("Structs")
+	poodle := Dog{"Poodle", 10}
+	fmt.Printf("%v\n", poodle)
+	poodle.Weight = 11
+	fmt.Printf("%v\n", poodle)
+}
+
+type Dog struct {
+	Breed  string
+	Weight int
 }
