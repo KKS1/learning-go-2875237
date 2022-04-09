d2cffd6abe6f656e5fee0b87412d0c397849e982
Kanwaljeet Singh Kalra <koolkalra@gmail.com>
2022-04-09 03:44:30 -0700

    Update main.go

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,28 @@ import (
 )
 
 func main() {
+	doSomething()
+	twoSum := addTwoValues(1, 2)
+	fmt.Println("Two values sum: ", twoSum)
+
+	allSum, lenValues := addAllValues(1, 2, 3, 4, 5)
+	fmt.Printf("All values sum and length: %v %v\n", allSum, lenValues)
+}
+
+func doSomething() {
 	fmt.Println("Functions")
+}
+
+func addTwoValues(val1, val2 int) int {
+	return val1 + val2
+}
+
+func addAllValues(values ...int) (int, int) {
+	total := 0
+
+	for _, val := range values {
+		total = total + val
+	}
+
+	return total, len(values)
 }
