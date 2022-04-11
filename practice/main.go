0e7f607f454e8b8f96c187316fae51840a50608e
Kanwaljeet Singh Kalra <koolkalra@gmail.com>
2022-04-11 16:17:10 -0700

    http read from GET url

--- practice/main.go
+++ practice/main.go
@@ -2,10 +2,31 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 const url = "http://services.explorecalifornia.org/json/tours.php"
 
 func main() {
-	fmt.Println("Network requests")
+	resp, err := http.Get(url)
+	checkError(err)
+
+	fmt.Printf("Response type: %T \n", resp)
+
+	defer resp.Body.Close()
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	checkError(err)
+
+	content := string(bytes)
+
+	fmt.Println(content)
+
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
