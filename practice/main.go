fb2257c9d0e9a33e700067121341c1f877787a1f
Kanwaljeet Singh Kalra <koolkalra@gmail.com>
2022-04-11 16:38:22 -0700

    http Get to json using decoder

--- practice/main.go
+++ practice/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 const url = "http://services.explorecalifornia.org/json/tours.php"
@@ -20,10 +23,44 @@ func main() {
 	defer resp.Body.Close()
 
 	bytes, err := ioutil.ReadAll(resp.Body)
+	checkError(err)
+
+	content := string(bytes)
+	fmt.Println(content)
+
+	tours := toursFromJson(content)
+
+	for _, tour := range tours {
+		price, err := strconv.ParseFloat(strings.TrimSpace(tour.Price), 64)
+		checkError(err)
+		fmt.Printf("Name: %v, Price: %v \n", tour.Name, price)
+	}
+}
+
+func toursFromJson(content string) []Tour {
+	tours := make([]Tour, 0, 20)
+
+	decoder := json.NewDecoder(strings.NewReader(content))
+
+	_, err := decoder.Token()
+	checkError(err)
+
+	var tour Tour
+
+	for decoder.More() {
+		err := decoder.Decode(&tour)
+		checkError(err)
+		tours = append(tours, tour)
+	}
+	return tours
+}
+
+type Tour struct {
+	Name, Price string
+}
+
+func checkError(err error) {
 	if err != nil {
 		panic(err)
 	}
-
-	content := string(bytes)
-	fmt.Print(content)
 }
